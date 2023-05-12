71b2c439a16548f76d868a21d357174cf3199ddd
yangsuwon <ysw7939@gmail.com>
2023-05-12 14:36:09 +0900

    .

---
+++ src/main/java/com/example/awsspring/AwsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsspring/AwsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
