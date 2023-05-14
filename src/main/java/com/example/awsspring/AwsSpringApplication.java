523dd64f26d830a619e75972e21e7bd574b4b4b2
yangsuwon <ysw7939@gmail.com>
2023-05-14 13:17:17 +0900

    Add deploy.sh

--- src/main/java/com/example/awsspring/AwsSpringApplication.java
+++ src/main/java/com/example/awsspring/AwsSpringApplication.java
@@ -2,12 +2,16 @@ package com.example.awsspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AwsSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsSpringApplication.class, args);
+		SpringApplication application = new SpringApplication(AwsSpringApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+
 	}
 
 }
